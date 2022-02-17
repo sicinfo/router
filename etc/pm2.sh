b6f22bc12c4111bb8e2a663b0f09ba9e465bdea8
moreira <moreiraxx+github-router@gmail.com>
2022-02-17 14:12:32 -0300

    atualiza

--- etc/pm2.sh
+++ etc/pm2.sh
@@ -3,9 +3,13 @@
 # incluir no arquivo rc.local
 # source ./pm2.sh
 
-[ -z $NVM_DIR ] && [ -s $HOME/.nvm/nvm.sh ] && \. $HOME/.nvm/nvm.sh
-
 function _pm2 {
+
+  [ -z $NVM_DIR ] && [ -s $HOME/.nvm/nvm.sh ] && \. $HOME/.nvm/nvm.sh
+
+  echo $NVM_DIR
+  
+  [ -z $NVM_DIR ] && exit 0  
   
   [[ $1 =~ ^((re)?start|stop)$ ]] || exit 0
 
@@ -13,14 +17,12 @@ function _pm2 {
   local PM2=$(which pm2)
   [ -z $PM2 ] || [ ! -x $PM2 ] && exit 0
 
-echo $PM2 - teste
-
   # cancela todas instances pm2
   [ stop = $1 ] && $PM2 -s kill && exit 0
 
   # verifica diretorio local
   local APPS="$HOME/etc/pm2"
-  [ -e $APPS ] || exit 0
+  [ -d $APPS ] || exit 0
 
   # iniciar pm2
   $PM2 -s ping
